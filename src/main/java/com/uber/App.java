dfa8dc9bfb49522b91343243647c1400279c363c
karthikeyiniD <dandukarthikeyini@gmail.com>
2023-05-04 09:52:16 +0530

    1

--- src/main/java/com/uber/App.java
+++ src/main/java/com/uber/App.java
@@ -1,13 +1,8 @@
 package com.uber;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
